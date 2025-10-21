ef6a18410f48de749732018cbd4ea3802505381d
Enzo Méresse <enzomresse1@gmail.com>
2025-10-21 09:45:19 +0200

    modif test

--- test_app.py
+++ test_app.py
@@ -4,9 +4,9 @@ import unittest
 from app import bonjour
 
 class TestApp(unittest.TestCase):
- def test_bonjour(self):
-    self.assertEqual(bonjour("Jenkins"), "Bonjour Jen  kins !")
-    self.assertEqual(bonjour("CI"), "Bonjour CI !")
+    def test_bonjour(self):
+        self.assertEqual(bonjour("Jenkins"), "Bonjour Jenkins !")
+        self.assertEqual(bonjour("CI"), "Bonjour CI !")
 
 if __name__ == "__main__":
- unittest.main()
+    unittest.main()
